15c216929bb5b0c0d5eeca014bd7a949bbaa446b
natayrasantos <natayra@kaskade.finance>
2024-09-23 18:11:38 +0200

    fix: sizes

--- src/components/ComingSoon.jsx
+++ src/components/ComingSoon.jsx
@@ -9,15 +9,21 @@ const ComingSoon = () => {
       justifyContent="center"
       alignItems="center"
       rowGap="2rem"
+      padding="5%"
     >
       <Grid item>
-        <Typography variant="h1" textAlign="center">
+        <Typography variant="h2" textAlign="center">
           Justyna Zurawska
         </Typography>
       </Grid>
       <Grid item>
-        <Typography variant="h2" textAlign="center" fontStyle="italic">
-          Coming Soon :)
+        <Typography variant="h5" textAlign="center" fontStyle="italic">
+          coaching@justynazurawska.com
+        </Typography>
+      </Grid>
+      <Grid item>
+        <Typography variant="h2" textAlign="center">
+          :)
         </Typography>
       </Grid>
     </Grid>
