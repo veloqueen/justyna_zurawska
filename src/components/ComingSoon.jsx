90f50f3225394b2af5fe069e55cd9995c45493d0
natayrasantos <natayra@kaskade.finance>
2024-09-23 18:02:31 +0200

    fix: center name and coming soon text

--- src/components/ComingSoon.jsx
+++ src/components/ComingSoon.jsx
@@ -2,9 +2,20 @@ import { Grid, Typography } from "@mui/material";
 
 const ComingSoon = () => {
   return (
-    <Grid container height="100vh" justifyContent="center" alignItems="center">
+    <Grid
+      container
+      height="100vh"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      rowGap="2rem"
+    >
       <Grid item>
-        <Typography variant="h1"> Justyna Zurawska</Typography>
+        <Typography variant="h1" textAlign="center">
+          Justyna Zurawska
+        </Typography>
+      </Grid>
+      <Grid item>
         <Typography variant="h2" textAlign="center" fontStyle="italic">
           Coming Soon :)
         </Typography>
