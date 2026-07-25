f0e0787af62ae7cfe6a6f9f2126fa91df3a22e07
veloqueen <75308876+veloqueen@users.noreply.github.com>
2026-07-25 14:08:00 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "@fontsource-variable/mulish";
 
 import { theme } from "./utils/theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import Home from "./components/Home";
+
 import ComingSoon from "./components/ComingSoon";
 
 function App() {
